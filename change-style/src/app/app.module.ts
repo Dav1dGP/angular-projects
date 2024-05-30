511bf9d14fafb2e04cb61e6fd4ecdc1d4eedffe5
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-05-30 17:31:20 +0200

    add comment module

--- change-style/src/app/app.module.ts
+++ change-style/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextoformatterComponent } from './textoformatter/textoformatter.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Sin esto no me funcionaban el tema de los input
 
 @NgModule({
   declarations: [
