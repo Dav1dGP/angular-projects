1b89f27dbdb981f33878acd5749049e334ccde7b
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-05-31 11:41:06 +0200

    add mock y dataType.ts

---
+++ list-music/src/assets/dataType.ts
@@ -0,0 +1,12 @@
+export interface DatosMusicales {
+    nombreBanda: string;
+    mejorAlbum: string;
+    anno: number;
+    cancionMasFamosa: string;
+    esArtista: boolean;
+    urlImagenArtista: string;
+    urlImagenAlbum: string;
+    ranking: number;
+  }
+  
+  
